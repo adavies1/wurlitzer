28ab08fabd3ba4378ea44148835bd5e8eef7267e
Alex Davies <alexdavieswebdev@gmail.com>
2020-05-06 22:51:45 +0100

    [Webpack] - Added devserver

---
+++ webpack-dev.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: 'ts-loader',
+                },
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx']
+    },
+};
