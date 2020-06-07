fb3b3095ed7ea6c44c22968253f12a40bd66acee
Alex Davies <alexdavieswebdev@gmail.com>
2020-06-07 23:33:57 +0100

    [Index] - Added more songs to choose from, and started using new MusicPlayer.load() function to re-use audio context / processors

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,38 @@ import load, { MusicPlayer } from './MusicPlayer';
 
 let player:MusicPlayer;
 
-const loadButton = document.getElementById('loadButton');
+const loadButtons = document.querySelectorAll('.loadButton');
 const playButton = document.getElementById('playButton');
 const pauseButton = document.getElementById('pauseButton');
 const stopButton = document.getElementById('stopButton');
 
-loadButton.addEventListener('click', () => {
-    load('test/unit/resources/Skid_Row2.mod')
-    .then(musicPlayer => {
-        console.log('ok!', musicPlayer);
-        player = musicPlayer;
-        loadButton.setAttribute('disabled', 'true');
-        playButton.removeAttribute('disabled');
-        pauseButton.removeAttribute('disabled');
-        stopButton.removeAttribute('disabled');
-    })
-    .catch(err => {
-        console.log('hmm...', err);
-    });
-});
+function loadSong(event: MouseEvent): void {
+    const loadFunc = typeof player === 'undefined' ? load : player.load;
+
+    if(event.target instanceof Element) {
+        if(typeof player === 'undefined') {
+            load(event.target.getAttribute('data-src'))
+                .then(musicPlayer => {
+                    player = musicPlayer;
+                    playButton.removeAttribute('disabled');
+                    pauseButton.removeAttribute('disabled');
+                    stopButton.removeAttribute('disabled');
+                })
+                .catch(err => {
+                    console.log('hmm...', err);
+                });
+        }
+        else {
+            player.load(event.target.getAttribute('data-src'))
+                .catch(err => {
+                    console.log('hmm...', err);
+                });
+        }
+    }
+};
+
+loadButtons.forEach(button => button.addEventListener('click', loadSong));
+
 playButton.addEventListener('click', () => {
     player.play();
 });
